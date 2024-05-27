faaa98bb0611be939fb75ed4d6d7a902baf9ab5a
Tom Liao <vicros@gmail.com>
2024-05-26 17:46:16 -0700

    Follow the expo navigation tutorial.
    https://docs.expo.dev/develop/file-based-routing/

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,16 @@
+import {Link, Stack} from 'expo-router';
+import {Text, View} from "react-native";
+
+export default function Settings() {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text>Settings</Text>
+        </View>
+    );
+}
