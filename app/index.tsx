cf53f2417ae3c59e9c9d4d6ee66e0d1e094e0be8
Tom Liao <vicros@gmail.com>
2024-05-26 17:30:23 -0700

    Run npm run reset-project.

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this Screen which for some reason is cutting off the last word, not sure what that's about.</Text>
+    </View>
+  );
+}
