65fa70c2dcdb0e60927e91e97418d8eddc67936f
Tom Liao <vicros@gmail.com>
2024-05-30 21:10:03 -0700

    Merge CreateItDialog.tsx into CreateItFAB.tsx. There was no benefit to keeping them separate and they are highly cohesive, so should be highly coupled as well.

--- src/its/CreateItFAB.tsx
+++ src/its/CreateItFAB.tsx
@@ -1,7 +1,8 @@
-import { AnimatedFAB } from 'react-native-paper';
-import CreateItDialog from '@/its/CreateItDialog';
-import { useState } from 'react';
+import { AnimatedFAB, Button, Dialog, TextInput } from 'react-native-paper';
+import { useContext, useState } from 'react';
 import { StyleSheet } from 'react-native';
+import { ItsContext } from '@/its/ItsContext';
+import useItsRepository from '@/its/useItsRepository';
 
 export default function CreateItFAB({ extended }: { extended: boolean }) {
     const [visible, setVisible] = useState(false);
@@ -17,6 +18,35 @@ export default function CreateItFAB({ extended }: { extended: boolean }) {
             />
             <CreateItDialog visible={visible} onDismiss={() => setVisible(false)} />
         </>
+    );
+}
+
+function CreateItDialog({ visible, onDismiss }: { visible: boolean; onDismiss: () => void }) {
+    const [name, setName] = useState('');
+    const { refreshIts } = useContext(ItsContext);
+    const itsRepository = useItsRepository();
+
+    // TODO(polish): Set a loading indicator after pressing Create
+    return (
+        <Dialog visible={visible} onDismiss={onDismiss}>
+            <Dialog.Content>
+                <TextInput label="Name" value={name} onChangeText={setName}></TextInput>
+            </Dialog.Content>
+            <Dialog.Actions>
+                <Button onPress={onDismiss}>Cancel</Button>
+                <Button
+                    onPress={async () => {
+                        await itsRepository.createIt(name);
+                        await refreshIts();
+                        onDismiss();
+                        setName('');
+                    }}
+                    mode="contained"
+                    disabled={name === ''}>
+                    Create
+                </Button>
+            </Dialog.Actions>
+        </Dialog>
     );
 }
 
