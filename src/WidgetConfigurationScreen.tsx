bd0f667cdaa2881d1f6561260ca5ef4bc4b9f9b0
Tom Liao <vicros@gmail.com>
2024-05-30 21:35:46 -0700

    Refactor the WidgetConfigurationScreen

--- src/WidgetConfigurationScreen.tsx
+++ src/WidgetConfigurationScreen.tsx
@@ -1,40 +1,72 @@
 import { WidgetConfigurationScreenProps } from 'react-native-android-widget';
-import { FlatList, View } from 'react-native';
-import { Button, Text } from 'react-native-paper';
+import { FlatList, StyleSheet, View } from 'react-native';
+import { Button, PaperProvider, Text } from 'react-native-paper';
 import ItWidget from '@/its/ItWidget';
-import * as SQLite from 'expo-sqlite';
-import { useEffect, useState } from 'react';
-import { ItsRepository } from '@/its/ItsRepository';
+import { useContext, useEffect } from 'react';
 import { It } from '@/its/It';
+import { initializeDb } from '@/db/initializeDb';
+import ItsContextProvider from '@/its/ItsContextProvider';
+import { SQLiteProvider } from 'expo-sqlite';
+import { ItsContext } from '@/its/ItsContext';
 
-export function WidgetConfigurationScreen({ widgetInfo, setResult, renderWidget }: WidgetConfigurationScreenProps) {
-    const [its, setIts] = useState<It[]>([]);
+export function WidgetConfigurationScreen(props: WidgetConfigurationScreenProps) {
+    return (
+        <SQLiteProvider databaseName="taplogit.db" onInit={initializeDb}>
+            <PaperProvider>
+                <ItsContextProvider>
+                    <ProvidedWidgetConfigurationScreen {...props} />
+                </ItsContextProvider>
+            </PaperProvider>
+        </SQLiteProvider>
+    );
+}
+
+function ProvidedWidgetConfigurationScreen({ widgetInfo, setResult, renderWidget }: WidgetConfigurationScreenProps) {
+    const { its, refreshIts } = useContext(ItsContext);
+
     useEffect(() => {
-        async function setup() {
-            const db = await SQLite.openDatabaseAsync('taplogit.db');
-            const result = await new ItsRepository(db).getIts();
-            setIts(result);
-        }
-
-        setup();
+        refreshIts();
     }, []);
 
     return (
-        <View>
-            <Text>Select an It</Text>
-            <Button onPress={() => setResult('cancel')}>Cancel</Button>
+        <View style={styles.container}>
+            <Text variant="titleLarge" style={styles.title}>
+                Configure your Tap Widget
+            </Text>
             <FlatList
                 data={its}
                 renderItem={({ item }) => (
-                    <Button
+                    <WidgetConfigurationScreenListItem
+                        it={item}
                         onPress={() => {
                             renderWidget(<ItWidget it={item} />);
                             setResult('ok');
-                        }}>
-                        {item.name}
-                    </Button>
+                        }}
+                    />
                 )}
+                style={styles.list}
             />
+            <Button onPress={() => setResult('cancel')}>Cancel</Button>
         </View>
     );
 }
+
+function WidgetConfigurationScreenListItem({ it, onPress }: { it: It; onPress: () => void }) {
+    return (
+        <Button mode="contained" onPress={onPress}>
+            {it.name}
+        </Button>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    title: {
+        textAlign: 'center',
+    },
+    list: {
+        margin: 12,
+    },
+});
