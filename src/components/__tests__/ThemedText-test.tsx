ef6fd9b5afbc8e19cfcfeca4c8f0225d58f0de87
Tom Liao <vicros@gmail.com>
2024-06-01 12:15:16 -0700

    Delete files left over from resetting the default expo project.

--- src/components/__tests__/ThemedText-test.tsx
+++
@@ -1,9 +0,0 @@
-import { ThemedText } from '../ThemedText';
-import * as React from 'react';
-import renderer from 'react-test-renderer';
-
-it(`renders correctly`, () => {
-    const tree = renderer.create(<ThemedText>Snapshot test!</ThemedText>).toJSON();
-
-    expect(tree).toMatchSnapshot();
-});
